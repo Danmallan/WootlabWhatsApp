1bcdb7e716e566755dcbcba90cedc71573363786
Danmallan <44886014+Danmallan@users.noreply.github.com>
2021-10-06 17:30:15 +0100

    - ChatList Fragment implemented - ChatActivity Created

--- app/src/main/java/com/example/wootlabwhatsapp/fragments/ChatsFragment.java
+++ app/src/main/java/com/example/wootlabwhatsapp/fragments/ChatsFragment.java
@@ -1,66 +1,164 @@
 package com.example.wootlabwhatsapp.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.wootlabwhatsapp.R;
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChatsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.wootlabwhatsapp.R;
+import com.example.wootlabwhatsapp.adapter.ChatsListAdapter;
+import com.example.wootlabwhatsapp.databinding.FragmentChatsBinding;
+import com.example.wootlabwhatsapp.model.Chats;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
 public class ChatsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String TAG = "ChatsFragment";
 
     public ChatsFragment() {
-        // Required empty public constructor
+//        Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChatsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChatsFragment newInstance(String param1, String param2) {
-        ChatsFragment fragment = new ChatsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private FirebaseUser firebaseUser;
+    private DatabaseReference reference;
+    private FirebaseFirestore firestore;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private Handler handler = new Handler();
+
+    private FragmentChatsBinding binding;
+    private List<Chats> list;
+    private ChatsListAdapter adapter;
+
+    private ArrayList<String> allUserID;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_chats, container, false);
+
+        list = new ArrayList<>();
+        allUserID = new ArrayList<>();
+
+        binding.chatRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new ChatsListAdapter(list, getContext());
+        binding.chatRecyclerView.setAdapter(adapter);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference();
+        firestore = FirebaseFirestore.getInstance();
+
+        if (firebaseUser != null) {
+            getChatList();
+        }
+
+        return binding.getRoot();
     }
+
+    private void getChatList() {
+        binding.progressCircular.setVisibility(View.VISIBLE);
+        list.clear();
+        allUserID.clear();
+
+
+        reference.child("ChatList").child(firebaseUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+                    String userID = Objects.requireNonNull(snapshot.child("chatid").getValue()).toString();
+                    Log.d(TAG, "onDataChange: userId" + userID);
+
+                    binding.progressCircular.setVisibility(View.GONE);
+                    allUserID.add(userID);
+                }
+                getUserData();
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+                Log.e(TAG, "onCancelled: "+ error.getMessage());
+
+            }
+        });
+    }
+
+    private void getUserData() {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                for(String userID : allUserID){
+                    firestore.collection("Users").document(userID).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            try{
+                                Chats chat = new Chats(
+                                        documentSnapshot.getString("userID"),
+                                        documentSnapshot.getString("userName"),
+                                        "This is a description",
+                                        "",
+                                        documentSnapshot.getString("imageProfile")
+                                );
+
+                                list.add(chat);
+                            }catch (Exception e){
+                                Log.e(TAG, "onSuccess: "+ e.getMessage());
+                            }
+
+                            if (adapter != null){
+                                adapter.notifyItemInserted(0);
+                                adapter.notifyDataSetChanged();
+                                Log.e(TAG, "onSuccess: "+ adapter.getItemCount());
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull @NotNull Exception e) {
+                            Log.e(TAG, "onFailure: "+ e.getMessage());
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+
+//
+//    public static List<Chats> getChatsList() {
+//
+//        List<Chats> list = new ArrayList<>();
+//        Chats chat = new Chats("01", "Damilola S", "Hi", "Yesterday", "");
+//        list.add(chat);
+//
+//        chat = new Chats("02", "Ishaq Zakari", "Hello", "now", "");
+//        list.add(chat);
+//
+//        chat = new Chats("03", "Yekeen Fatai", "Greetings", "Tuesday", "");
+//        list.add(chat);
+//
+//        return list;
+//    }
 }
